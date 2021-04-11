f6b541adebc81549e1db2faf6fe1a74f3581134d
programisitch <alexasda051@gmail.com>
2021-04-11 12:36:07 +0300

    add TreeMap to Tester.java

--- src/tester/Tester.java
+++ src/tester/Tester.java
@@ -1,4 +1,12 @@
 package tester;
 
+import dto.Product;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Tester {
+    TreeMap<String, TreeMap<String, Product>> product = new TreeMap<>();
+
+
 }
