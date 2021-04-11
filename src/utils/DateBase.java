73dcafae8cc7f459010d3657d12fd9b007d1ff8b
programisitch <alexasda051@gmail.com>
2021-04-11 12:35:24 +0300

    add methods for DB

--- src/utils/DateBase.java
+++ src/utils/DateBase.java
@@ -1,16 +1,65 @@
 package utils;
 
 import dto.Product;
-import java.util.HashMap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Convert csv to java class
+ */
 public class DateBase {
 
-    private String path;
-    private final HashMap<String, HashMap<String, Product>> product = new HashMap<>();
-
-
-    public DateBase(String path) {
-        this.path = path;
+    public void addProduct(TreeMap<String, TreeMap<String, Product>> product, Product productItem){
+        product.putIfAbsent(productItem.getGroup(), new TreeMap<>());
+        product.get(productItem.getGroup()).putIfAbsent(productItem.getName(), productItem);
     }
+
+    public void editProduct(TreeMap<String, TreeMap<String, Product>> product, Product productItem){
+        product.get(productItem.getGroup()).put(productItem.getName(), productItem);
+    }
+
+    public void deleteProduct(TreeMap<String, TreeMap<String, Product>> product, Product productItem){
+        product.get(productItem.getGroup()).remove(productItem.getName(), productItem);
+    }
+
+    public void addGroupProduct(TreeMap<String, TreeMap<String, Product>> product, String group){
+        product.putIfAbsent(group, new TreeMap<>());
+    }
+
+    public void editGroupProduct(TreeMap<String, TreeMap<String, Product>> product,  String group){
+        product.put(group, new TreeMap<>());
+    }
+
+    public void deleteGroupProduct(TreeMap<String, TreeMap<String, Product>> product, String group){
+        product.remove(group);
+    }
+
+    public boolean checkGroup(TreeMap<String, TreeMap<String, Product>> product, String group){
+        boolean checkable = false;
+        for(Map.Entry<String, TreeMap<String, Product>> productItemGroup : product.entrySet()){
+            if(productItemGroup.getKey().equals(group)){
+                checkable = true;
+                break;
+            }
+        }
+        return checkable;
+    }
+
+    public boolean checkNameProduct(TreeMap<String, TreeMap<String, Product>> product, String name){
+        boolean checkable = false;
+        for(Map.Entry<String, TreeMap<String, Product>> productItemGroup : product.entrySet()){
+            for(Map.Entry<String, Product> productItemName : productItemGroup.getValue().entrySet()){
+                if (productItemName.getKey().equals(name)){
+                    checkable = true;
+                    break;
+                }
+            }
+        }
+        return checkable;
+    }
+
 }
 
