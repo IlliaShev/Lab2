f0f125055503b3f05bfa283509354ff71a5b864d
programisitch <alexasda051@gmail.com>
2021-04-17 18:29:11 +0300

    javaDoc

---
+++ src/javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Interface","l":"ChangeValueWindow"},{"p":"Interface","l":"DataWindow"},{"p":"utils","l":"DateBase"},{"p":"dto","l":"GroupOfProducts"},{"p":"dto","l":"HandlerGroupOfProducts"},{"p":"dto","l":"Product"}];updateSearchResults();
